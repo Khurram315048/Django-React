467526706f829aa4477e4444f2f952de5578a5cf
Muhammad Khurram <saleemkhurram420@gmail.com>
2026-07-18 17:10:31 +0500

    Updating django react project

---
+++ frontend/src/components/Note.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/Note.css"
+
+
+function Note({note,onDelete}){
+    const formattedDate=new Date(note.created_at).toLocaleDateString("en-US")
+    
+    return (
+        <div className="note-container">
+            <p className="note-tilte">{note.tite}</p>
+            <p className="note-content">{note.content}</p>
+            <p className="note-date">{formattedDate}</p>
+            <button className="delete-button" onClick={() =>onDelete(note.id)}>
+                Delete Note
+            </button>
+        </div>
+    );
+}
+
+export default Note;
---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+function NotFound(){
+    return <div>
+        <h1>The page you are looking for is not found!!</h1>
+        
+    </div>
+}
+
+export default NotFound
